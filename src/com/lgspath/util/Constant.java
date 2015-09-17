48d69328dfae6b3e856aa2931669ec206b2f5d96
lgspath <1370901490@qq.com>
2015-09-17 12:23:28 +0800

    作业提交

---
+++ src/com/lgspath/util/Constant.java
@@ -0,0 +1,12 @@
+package com.lgspath.util;
+
+/**
+ * 游戏项目中用到的常量
+ * @author 兔八哥
+ *
+ */
+public class Constant {
+	public static final int GAME_WIDTH = 500;
+	public static final int GAME_HEIGHT = 500;
+	
+}
